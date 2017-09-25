c7d7645640f53b999bf16bcdd86750276d7a3a17
Erik Mattfolk <erik.mattfolk@gmail.com>
2017-09-25 15:57:04 +0200

    Added enemy functionality

---
+++ Color TD/Dot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Color_TD
+{
+    class Dot
+    {
+        private static Bitmap image = new Bitmap("..\\..\\Black_dot.png"); //TODO: move to subclasses
+
+        private int speed, size;
+        private double distance;
+        private PointD position;
+
+        public Dot (int speed, int size, double distance)
+        {
+            this.speed = speed;
+            this.size = size;
+            this.distance = distance;
+            position = new PointD();
+        }
+
+        public double UpdateDistance (double deltaTime)
+        {
+            distance += deltaTime * speed;
+            return distance;
+        }
+
+        public PointD Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public static Bitmap Image
+        {
+            get { return image; }
+        }
+    }
+}
--- Color TD/MainForm.cs
+++ Color TD/MainForm.cs
@@ -16,6 +16,7 @@ namespace Color_TD
     public partial class MainForm : Form
     {
         private static readonly int FPS = 60, SLEEPTIME = 1000/FPS;
+        private double lastUpdateTime;
         private Bitmap canvas;
         private TDMap map;
         private Stopwatch stopWatch;
@@ -25,6 +26,26 @@ namespace Color_TD
         {
             InitializeComponent();
             Application.Idle += GameLoop;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+            stopWatch = new Stopwatch();
+            canvas = new Bitmap(480, 480);
+            enemies = new List<Dot>() { new Dot(100, 32, 0) };
+            map = new TDMap("..\\..\\Map1.png", new Point[] {
+                new Point(490,69),
+                new Point(67,69),
+                new Point(67,175),
+                new Point(395,175),
+                new Point(395,280),
+                new Point(63,280),
+                new Point(63,417),
+                new Point(490,417)
+            });
+            stopWatch.Start();
+            lastUpdateTime = stopWatch.Elapsed.TotalSeconds;
         }
 
         private void GameLoop(object sender, EventArgs e)
@@ -45,6 +66,9 @@ namespace Color_TD
 
         private void GameUpdate()
         {
+            double deltaTime = stopWatch.Elapsed.TotalSeconds - lastUpdateTime;
+            lastUpdateTime = stopWatch.Elapsed.TotalSeconds;
+            UpdatePositions(deltaTime);
         }
 
         private void Render()
@@ -52,26 +76,22 @@ namespace Color_TD
             using (Graphics g = Graphics.FromImage(canvas))
             {
                 g.DrawImage(map.Map, 0, 0);
+                foreach (Dot enemy in enemies)
+                {
+                    float correction = enemy.Size / 2f;
+                    g.DrawImage(Dot.Image, (float)(enemy.Position.X - correction), (float)(enemy.Position.Y - correction), enemy.Size, enemy.Size);
+                }
                 Refresh();
             }
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void UpdatePositions(double deltaTime)
         {
-            DoubleBuffered = true;
-            stopWatch = new Stopwatch();
-            canvas = new Bitmap(480,480);
-            map = new TDMap("..\\..\\Map1.png", new Point[] {
-                new Point(490,69),
-                new Point(67,69),
-                new Point(67,175),
-                new Point(395,175),
-                new Point(395,280),
-                new Point(63,280),
-                new Point(63,417),
-                new Point(490,417)
-            });
-            stopWatch.Start();
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                double distance = enemies[i].UpdateDistance(deltaTime);
+                enemies[i].Position = map.GetPosition(distance);
+            }
         }
 
         [StructLayout(LayoutKind.Sequential)]
